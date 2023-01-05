b6a9ee492a3d5529cbe68dec6c1450a66f680379
Nicolas Sawaya <nicolas.sawaya@intel.com>
2023-01-05 14:25:34 -0800

    setup

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ README = (HERE / "README.md").read_text()
 setup(
     name="qtn-design",  # Replace with your own username
     version="0.0.1",
-    author="Nicolas P. D. Sawaya",
+    author="Matt Thibodeau, Nicolas Sawaya",
     author_email="nicolas.sawaya@intel.com",
-    description="Package for encoding matrices into qubits.",
+    description="Automated quantum tensor network design.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/mat2qubit",
+    url="https://github.com/intel-sandbox/applications.quantum.pando-tn",
     license="Apache 2",
     # package_dir={'': 'src'},
     packages=["qtn-design"],
@@ -28,12 +28,9 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7", # required for ordered dicts.
-    install_requires=["sympy>=1.3", "scipy>=1.1", "openfermion>=1.0"],
+    install_requires=["quimb>=1.4", "scipy>=1.1", "torch>=1.12"],
     extras_require={
         "dev": [
-            "black~=22.3.0",
-            "flake8~=4.0.1",
-            "isort~=5.10.1",
         ]
     },
 )
